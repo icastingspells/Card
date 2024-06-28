6a736222c38c16e211a0cf208341e816aff4f537
Kira <kirillpopov385@mail.ru>
2024-06-28 15:26:29 +0300

    second

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const block = document.querySelector('.main');
 const text = document.querySelectorAll('.text');
 
-const originalShadow = getComputedStyle(text[0]).textShadow;
+const originalShadow = "0px 2px 3px rgba(0, 0, 0, 0.6)";
 
 block.addEventListener('mousemove', function(e) {
-  const tiltAmount = 50;
+  const tiltAmount = 70;
   const shadowStrength = 10;
   const tiltX = (e.clientX / window.innerWidth - 0.5) * tiltAmount;
-  const tiltY = (e.clientY / window.innerHeight - 0.4) * tiltAmount*2;
+  const tiltY = (e.clientY / window.innerHeight - 0.6) * tiltAmount*2;
   text.forEach(text => {
   const shadowY = (e.clientY / window.innerHeight - 0.4) * shadowStrength;
   const shadowX = (e.clientX / window.innerWidth - 0.5) * -shadowStrength;
